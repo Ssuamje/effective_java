e3ba14707f5daa9337457fa94246a943506492b1
Ssuamje <side2850@naver.com>
2023-07-12 19:46:07 +0900

    Item 17 - 변경 가능성을 최소화하라

--- src/test/java/my/effective/java/chapter4/item17/ImmutableTest.java
+++ src/test/java/my/effective/java/chapter4/item17/ImmutableTest.java
@@ -15,6 +15,20 @@ public class ImmutableTest {
 		InstanceOnHeap defensive = immutable.getMutableCountDefensively();
 		InstanceOnHeap defensive2 = immutable.getMutableCountDefensively(); // 방어적 복사(스냅샷)이기 때문에 다른 인스턴스다.
 		assertNotEquals(defensive.hashCode(), defensive2.hashCode());
+		/*------------------------------------함수형 따라해보기-----------------------------------------*/
+		long startTime = System.currentTimeMillis();
+		for (int i = 0; i < 1000000; i++) {
+			immutable = immutable.functionalLikeImmutableAdd();
+		}
+		long endTime = System.currentTimeMillis();
+
+		System.out.println("함수형 따라하기 : " + (endTime - startTime) + "ms"); // 8ms
+		startTime = System.currentTimeMillis();
+		for (int i = 0; i < 1000000; i++) {
+			immutable.increaseMutableCount();
+		}
+		endTime = System.currentTimeMillis();
+		System.out.println("그냥 더하기 : " + (endTime - startTime) + "ms"); // 4ms
 	}
 
 	@Test
