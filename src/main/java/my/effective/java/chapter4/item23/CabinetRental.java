7cac712e37cfecd092fbe79c84cc3adb134689c3
Ssuamje <side2850@naver.com>
2023-07-20 19:15:01 +0900

    Item 22 - 인터페이스는 타입을 정의하는 용도로만 사용하라

---
+++ src/main/java/my/effective/java/chapter4/item23/CabinetRental.java
@@ -0,0 +1,14 @@
+package my.effective.java.chapter4.item23;
+
+public class CabinetRental {
+
+	public static final int RENTAL_DAYS = 100;
+
+	public void rentCabinet() {
+		System.out.println("캐비넷을 빌렸습니다.");
+	}
+
+	public void returnCabinet() {
+		System.out.println("캐비넷을 반납했습니다.");
+	}
+}
---
+++ src/main/java/my/effective/java/chapter4/item23/WrongCabinetRental.java
@@ -0,0 +1,9 @@
+package my.effective.java.chapter4.item23;
+
+public interface WrongCabinetRental {
+	int RENTAL_DAYS = 100;
+
+	void rentCabinet();
+
+	void returnCabinet();
+}
