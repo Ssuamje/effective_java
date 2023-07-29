a37b8c0a29f17c0a2149a38a6f97c901f27eb821
Ssuamje <side2850@naver.com>
2023-07-29 18:19:43 +0900

    Item 44 - 표준 함수형 인터페이스를 사용하라

--- src/main/java/my/effective/java/chapter7/item44/DomainEntity.java
+++ src/main/java/my/effective/java/chapter7/item44/DomainEntity.java
@@ -8,6 +8,8 @@ public class DomainEntity implements Validatable {
 	private final String name;
 	private final Validator<String> validator;
 
+	// validation의 경우에는 외부에서 변경될 로직이 아니므로 상황과 맞지 않지만,
+	// 그냥 연습삼아서 내부 로직을 변경할 수 있도록 주입받게끔 구성해보았음
 	public DomainEntity(String name, Validator<String> validator) {
 		this.name = name;
 		this.validator = validator;
